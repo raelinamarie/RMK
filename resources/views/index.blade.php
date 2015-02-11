f9712ab75ca891081f6422aef5f590785f0666b9
raelinamarie <raelinamarie@gmail.com>
2015-02-11 13:01:04 -0700

    responsive fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,12 @@
 @include('components.head')
-<div class="flex-center quote-pad">
-    <div class="col-lg-1">
+<div class="flex-center">
+    <div class="col-lg-2 col-xs-4 page-start">
         <i class="fa fa-paper-plane-o fa-5x"></i>
     </div>
-    <div class="page-start col-lg-6 col-md-6 col-sm-7 col-xs-8 blockquote-reverse">
+    <div class="page-start col-lg-5 col-md-6 col-xs-12 blockquote-reverse">
         <h1>Live in the future, <br>then build what’s missing.</h1>
         <br>
         <h3><i>-Paul Graham</i></h3>
     </div>
+
 </div>
