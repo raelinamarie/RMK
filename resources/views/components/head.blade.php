132c7f98cd945433d2087ed3e40fb3213269e988
raelinamarie <raelinamarie@gmail.com>
2015-02-11 12:44:18 -0700

    head in place stylesheet updated filler page done!

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>{{'Thoughts & Ramblings'}}</title>
+
+<!-- GLOBAL STYLES -->
+<link href="/css/app.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:=100' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,11 @@
-Hello World!
+@include('components.head')
+<div class="flex-center quote-pad">
+    <div class="col-lg-1">
+        <i class="fa fa-paper-plane-o fa-5x"></i>
+    </div>
+    <div class="page-start col-lg-6 col-md-6 col-sm-7 col-xs-8 blockquote-reverse">
+        <h1>Live in the future, <br>then build what’s missing.</h1>
+        <br>
+        <h3><i>-Paul Graham</i></h3>
+    </div>
+</div>
