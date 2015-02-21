3bd6de4d6b8e9ddf824738d6d0e4b781a2f262ff
raelinamarie <raelinamarie@gmail.com>
2015-02-21 13:29:04 -0700

    remade contact page, responsive tested typeform works

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.main')
 @section('content')
     <div class="container flexcontainer">
-        <div class="col-lg-8 col-xs-12">
-                <h1><del>I"m available for freelance work!</del></h1>
-                <h3>I'm sorry, at this time I am occupied with other work and will not be available to start any new projects. You're more than welcome to send me an email and we can get the conversation started. Or if you just want to say hi.</h3>
+        <div class="col-md-8 col-xs-12">
+                <h1><del>I'm available for freelance work!</del></h1>
+                <h3>Sorry, at this time I am occupied with other work and will not be available to start any new projects.
+                    <br><br>
+                    If you're interested in doing a project together, send me an email and we can get the conversation started.</h3>
             </div>
-        <div class="col-xs-4 flex-center">
-            <button class="main">
-                <a href="/contact-form">
+        <div class="col-xs-5 flex-center nav">
+            <a href="/contact-form">
                     <i class="fa fa-envelope"></i>
-                </a>
-                <b>Email Me</b>
-            </button>
+                <b>Email Me</b></a>
         </div>
     </div>
 @stop
