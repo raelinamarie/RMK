74157ef066591971a1c1be66af7b5310a284ed16
raelinamarie <raelinamarie@gmail.com>
2015-03-08 23:30:24 -0600

    bikesurf detail page

---
+++ resources/views/pages/detail-bikesurf.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('content')
+    <div class="col-xs-12 flex-center">
+        <img class="img-responsive img-img img" src="http://i.imgur.com/vaJPy1i.png" alt="BikeSurf Infographic">
+    </div>
+    <div class="col-xs-8 col-xs-offset-2 flex-center">
+        <p>Bikesurf is a bike sharing platform based in Berlin, Germany. I volunteer for the organization by
+            creating assets such as this one to promote the organization in general and for specific events.</p>
+    </div>
+@stop
---
+++ resources/views/pages/detail-vistalife.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('content')
+    <div class="col-lg-4 col-xs-12 pull-right text-right sm-text grey">
+        <i>DrawClerk was designed while working for AppIt Ventures LLC</i>
+    </div>
+@stop
