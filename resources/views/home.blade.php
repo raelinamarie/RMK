28d0f8aad5f6c3d1c23919f9b91da9e315835560
raelinamarie <raelinamarie@gmail.com>
2015-05-29 14:43:24 -0600

    homepage font  update for funzies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <i class="fa fa-paper-plane-o fa-5x"></i>
         </div>
         <div class="flex-container col-lg-8 col-xs-12">
-            <h1>Live in the future,<br>then build what’s missing.</h1>
+            <h1 class="codystar">Live in the future,<br>then build what’s missing.</h1>
             <br>
             <h3><i>-Paul Graham</i></h3>
         </div>
