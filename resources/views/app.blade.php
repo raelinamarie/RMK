0194f8aa20f76c969ad7d31c38f43a8d63ae31cf
raelinamarie <raelinamarie@gmail.com>
2015-09-09 16:43:38 -0600

    font updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,8 @@
 	<link href="/css/app.css" rel="stylesheet">
 
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Codystar' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Yeseva+One' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
