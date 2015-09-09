6549cabf0273e3bd4d9d07935c495f7a9c4ff0f2
raelinamarie <raelinamarie@gmail.com>
2015-09-09 17:01:17 -0600

    homepage addition

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,17 @@
 
 @section('content')
     <div class="flexcontainer container">
+       @include('partials.intro-quote')
         <div class="col-xs-12 flex-center">
-            <i class="fa fa-paper-plane-o fa-5x"></i>
-        </div>
-        <div class="flex-container col-lg-8 col-xs-12">
-            <h1 class="fancy">Live in the future,<br>then build what’s missing.</h1>
-            <br>
-            <h3><i>-Paul Graham</i></h3>
+            <h1 class="fancy">
+                "How to Train a Chicken" is now out!
+            </h1>
+            <div class="col-xs-12 col-sm-8">
+                <p>After many long nights and revisions, I'm happy to say that my first ever children's book ahs been published! You can take a look on iTunes and buy yourself a copy for only $5.</p>
+            </div>
+            <h3 class="fancy">
+                <a href="https://itunes.apple.com/us/book/how-to-train-a-chicken/id1033429795?mt=11">Check it out!</a>
+            </h3>
         </div>
     </div>
 @stop
---
+++ resources/views/partials/intro-quote.blade.php
@@ -0,0 +1,8 @@
+<div class="col-xs-12 flex-center">
+    <i class="fa fa-paper-plane-o fa-5x"></i>
+</div>
+<div class="flex-container col-lg-8 col-xs-12">
+    <h1 class="fancy">Live in the future,<br>then build what’s missing.</h1>
+    <br>
+    <h3><i>-Paul Graham</i></h3>
+</div>
