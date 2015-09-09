9a5d9bd9f41d9e8b24390a26bdab3661ca857f88
raelinamarie <raelinamarie@gmail.com>
2015-09-09 17:05:46 -0600

    homepage addition

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,18 @@
 
 @section('content')
     <div class="flexcontainer container">
-       @include('partials.intro-quote')
+       {{--@include('partials.intro-quote')--}}
+
+        <h1 class="fancy">
+            "How to Train a Chicken" is now out!
+        </h1>
         <div class="col-xs-12 flex-center">
-            <h1 class="fancy">
-                "How to Train a Chicken" is now out!
-            </h1>
             <div class="col-xs-12 col-sm-8">
-                <p>After many long nights and revisions, I'm happy to say that my first ever children's book ahs been published! You can take a look on iTunes and buy yourself a copy for only $5.</p>
+                <p>After many long nights and revisions, I'm happy to say that my first ever children's book has been published! You can take a look on iTunes and buy yourself a copy for only $5.</p>
             </div>
-            <h3 class="fancy">
-                <a href="https://itunes.apple.com/us/book/how-to-train-a-chicken/id1033429795?mt=11">Check it out!</a>
-            </h3>
         </div>
+        <h3 class="fancy">
+            <a href="https://itunes.apple.com/us/book/how-to-train-a-chicken/id1033429795?mt=11">Check it out!</a>
+        </h3>
     </div>
 @stop
